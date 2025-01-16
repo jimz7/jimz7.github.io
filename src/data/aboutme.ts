33c5f7e1481a24b7f238a2924278a9a606788729
jimz7 <zjzwin@126.com>
2025-01-15 23:14:32 -0600

    first draft

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -18,22 +18,23 @@ export interface AboutMe {
 }
 
 export const aboutMe: AboutMe = {
-  name: "Jane R. Smith",
-  title: "Ph.D. Candidate",
-  institution: "Stanford University",
+  name: "Jinze Zhao",
+  title: "M.S. student",
+  institution: "University of Texas at Austin",
   // Note that links work in the description
   description:
-    "I'm a final-year <a href='https://www.stanford.edu'>PhD candidate</a> working at the intersection of causal inference and machine learning. My research focuses on developing robust, interpretable systems that can reason about cause and effect in complex environments.",
-  email: "______@stanford.edu",
-  imageUrl:
-    "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
-  githubUsername: "janesmith",
-  linkedinUsername: "janesmith",
-  twitterUsername: "janesmith",
-  blogUrl: "https://",
-  cvUrl: "https://",
-  institutionUrl: "https://www.stanford.edu",
+    "I’m a graduate student pursuing M.S. in <a href='https://www.ece.utexas.edu/'>Electrical and Computer Engineering</a> at <a href='https://www.utexas.edu/'>the University of Texas at Austin</a>. Previously, I received B.S. in Electrical and Computer Engineering and B.S. in Mathematics (with Honors) from UT Austin.<br><br>My research focuses on Machine Learning, especially understanding the theoretical foundations and improving the generalization of models, such as diffusion models and mixture-of-experts models. Currently, I work as a Research Assistant at <a href='https://vita-group.github.io/group.html/'>VITA</a> lab.",
+    // I'm a final-year <a href='https://www.stanford.edu'>PhD candidate</a> working at the intersection of causal inference and machine learning. My research focuses on developing robust, interpretable systems that can reason about cause and effect in complex environments.",
+  email: "jz24694@utexas.edu",
+  // imageUrl:
+  //   "/images/profile.png",
+  googleScholarUrl: "https://scholar.google.com/citations?user=fIIlS-0AAAAJ",
+  githubUsername: "jimz7",
+  linkedinUsername: "jinze-zhao",
+  // twitterUsername: "janesmith",
+  // blogUrl: "https://",
+  // cvUrl: "https://",
+  // institutionUrl: "https://www.stanford.edu",
   // altName: "",
-  // secretDescription: "I like dogs.",
+  secretDescription: '"Not all who wander are lost." - Prophet Velen, Hearthstone',
 };
--- src/data/publication.ts
+++ src/data/publication.ts
@@ -15,25 +15,41 @@ export const publicationData: Publication[] = [
   // If you don't want to show publications, just make the array empty.
   {
     year: "2024",
-    conference: "NeurIPS",
-    title: "Scalable Causal Discovery in High-Dimensional Time Series",
-    authors: "Jane Smith, Sarah Johnson, Yue Zhang",
-    paperUrl: "https://arxiv.org/abs/2409.15476",
-    codeUrl: "https://github.com/jsmith/scalable-causal-discovery",
+    conference: "NeurIPS 2024 Workshop on Compositional Learning: Perspectives, Methods, and Paths Forward",
+    title: "Enhancing Generalization in Sparse Mixture of Experts Models: The Case for Increased Expert Activation in Compositional Tasks",
+    authors: "Jinze Zhao",
+    paperUrl: "https://arxiv.org/abs/2410.13964",
+    // codeUrl: "https://github.com/jsmith/scalable-causal-discovery",
     //bibtex: "https://arxiv.org/abs/2409.15476.bib",
-    tldr: "Using causal discovery to find the causal structure of high-dimensional time series data.",
-    imageUrl:
-      "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    award: "🏆 Best Paper Award",
+    // tldr: "Using causal discovery to find the causal structure of high-dimensional time series data.",
+    // imageUrl:
+    //   "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    // award: "🏆 Best Paper Award",
     // if you have an image in public/images, you can use it like this:
     // imageUrl: "/images/publication-image.jpg"
   },
   {
+    year: "2024",
+    conference: "ICLR 2024 Workshop on Mathematical and Empirical Understanding of Foundation Models",
+    title: "Generalization Error Analysis for Sparse Mixture-of-Experts: A Preliminary Study",
+    authors: "Jinze Zhao, Peihao Wang, Zhangyang Wang",
+    paperUrl: "https://arxiv.org/abs/2403.17404",
+    // codeUrl: "https://github.com/jsmith/robust-causal-discovery",
+  },
+  {
     year: "2023",
-    conference: "ICML",
-    title: "Robust Causal Discovery Under Distribution Shift",
-    authors: "Jane Smith, Xue Chen, Sarah Johnson",
-    paperUrl: "https://arxiv.org/abs/2302.13095",
-    codeUrl: "https://github.com/jsmith/robust-causal-discovery",
+    conference: "arXiv preprint",
+    title: "Meta ControlNet: Enhancing Task Adaptation via Meta Learning",
+    authors: "Junjie Yang, Jinze Zhao, Peihao Wang, Zhangyang Wang, Yingbin Liang",
+    paperUrl: "https://arxiv.org/abs/2312.01255",
+    codeUrl: "https://anonymous.4open.science/r/meta_controlnet-4E0C/README.md",
+  },
+  {
+    year: "2023",
+    conference: "arXiv preprint",
+    title: "CrossEAI: Using Explainable AI to generate better bounding boxes for Chest X-ray images",
+    authors: "Jinze Zhao",
+    paperUrl: "https://arxiv.org/abs/2310.19835",
+    // codeUrl: "https://anonymous.4open.science/r/meta_controlnet-4E0C/README.md",
   },
 ];
